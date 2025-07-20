980a02b1e76d0cfcd1d3b5769a442fffc49e7d66
whoami-saloni <148846161+whoami-saloni@users.noreply.github.com>
2025-07-20 15:30:29 +0530

    Fix the bad gateway error

--- app.py
+++ app.py
@@ -61,4 +61,5 @@ def evaluation():
         }
     return render_template("evaluation.html", metrics=metrics)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
